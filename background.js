de7c97e36578620127f68978f135ae01c24f0cd1
guest271314 <guest271314@gmail.com>
2026-04-12 17:58:53 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,56 +1,10 @@
-async function handleEvent(tab, sender, sendResponse) {
-  const url = sender ? sender.origin : new URL(tab.url).origin;
-  if (tab === 'abort') {
-    try {
-      console.log(await fetch('http://localhost:8000', {
-        method: 'head',
-        cache: 'no-store'
-      }));
-    } catch (e) {
-      console.log(e);
-      return false;
-    }
-  }
-  await chrome.declarativeNetRequest.updateSessionRules({
-    addRules: [{
-      "id": 1,
-      "priority": 1,
-      "action": {
-        "type": "modifyHeaders",
-        "responseHeaders": [{
-          "header": "content-security-policy",
-          "operation": "remove"
-        }, {
-          "header": "content-security-policy-report-only",
-          "operation": "remove"
-        }, {
-          "header": "cross-origin-embedder-policy",
-          "operation": "remove"
-        }]
-      },
-      "condition": {
-        "urlFilter": url,
-        "resourceTypes": [
-          "main_frame", 
-          "sub_frame", 
-          "script", 
-          "object", 
-          "xmlhttprequest", 
-          "csp_report", 
-          "other"
-        ]
-      }
-    }],
-    removeRuleIds: [1]
-  });
-  console.log(await await chrome.declarativeNetRequest.getSessionRules());
-  await chrome.runtime.sendNativeMessage('nm_php', {});
-}
+globalThis.name = chrome.runtime.getManifest().short_name;
 
-chrome.runtime.onInstalled.addListener((e)=>console.log(e));
+globalThis.port = chrome.runtime.connectNative(globalThis.name);
+port.onMessage.addListener((message) => console.log(message));
+port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
+port.postMessage(new Array(209715));
 
-chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e)=>console.log(e));
-
-chrome.action.onClicked.addListener(handleEvent);
-
-chrome.runtime.onMessageExternal.addListener(handleEvent);
+chrome.runtime.onInstalled.addListener((reason) => {
+  console.log(reason);
+});
